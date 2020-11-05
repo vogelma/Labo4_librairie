45f314801d12331bafc862436ddfaddba4fb1af6
Maelle <maelle.vogel@heig-vd.ch>
2020-11-05 15:15:34 +0100

    tab

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------------
 Nom du fichier : main.cpp
-Auteur(s)      : Maëlle Vogel
-Date creation  : <jj.mm.aaaa>
+Auteur(s)      : Maëlle Vogel et Valentin Chételat
+Date creation  : 05.11.2020
 Description    : <à compléter>
 Remarque(s)    : <à compléter>
 Compilateur    : Mingw-w64 g++ 8.1.0
@@ -13,6 +13,7 @@ Compilateur    : Mingw-w64 g++ 8.1.0
 
 #define EMPTY_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n');
 using namespace std;
+
 int main() {
 
     cout << "Presser ENTER pour quitter";
