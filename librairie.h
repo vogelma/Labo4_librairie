ae722b31ec45a3d116f0218fb3d4e899b1f08f00
valentinchetelat <71428053+valentinchetelat@users.noreply.github.com>
2020-11-06 18:42:15 +0100

    Update librairie.h

--- librairie.h
+++ librairie.h
@@ -11,7 +11,12 @@ Compiler      : Mingw-w64 g++ 8.1.0
 #define LABO4_LIBRAIRIE_LIBRAIRIE_H
 
 bool isEven(int number);
-int sumNumber(int number);
-bool isPrime(int number);
+int sumNumbers(int number);
+bool isPrime(unsigned int number);
+bool nbreArmstrong(unsigned int number);
+int buffer(string bufferToIterate, char& smallestLowerLetter, char& biggestUpperLetter);
+void trigo(double deg, double& sinus, double& cosinus, double& tangent);
+bool answerYes(string question, const char YES, const char NO);
+int random(int minValue, int maxValue);
 
 #endif //LABO4_LIBRAIRIE_LIBRAIRIE_H
