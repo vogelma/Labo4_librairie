8a0d24cd786a3b64cfdb804370b8695f8950ca0b
valentinchetelat <71428053+valentinchetelat@users.noreply.github.com>
2020-11-09 18:46:48 +0100

    Update librairie.h

--- librairie.h
+++ librairie.h
@@ -3,21 +3,21 @@
 
 #include <string>
 
-bool    isEven              (unsigned int number);
-int     sumDigits           (unsigned int number);
-bool    isPrime             (unsigned int number);
-bool    nbreArmstrong       (unsigned int number);
-int     buffer              (std::string& bufferToIterate,
-                             char& smallestLowerLetter,
-                             char& biggestUpperLetter);
-void    trigo               (const double deg,
-                             double& sinus,
-                             double& cosinus,
-                             double& tangent);
-bool    answerYes           (const std::string question,
-                             const char YES,
-                             const char NO);
-int     random              (const int minValue,
-                             const int maxValue);
+bool    isEven          (const unsigned int NUMBER);
+int     sumDigits       (unsigned int number);
+bool    isPrime         (const unsigned int NUMBER);
+bool    nbArmstrong     (const unsigned int NUMBER);
+int     buffer          (std::string& bufferToIterate,
+                         char& smallestLowerLetter,
+                         char& biggestUpperLetter);
+void    trigo           (const double ANGLE,
+                         double& sine,
+                         double& cosine,
+                         double& tangent);
+bool    answerYes       (const std::string QUESTION,
+                         const char YES,
+                         const char NO);
+int     random          (const int MINIMUM,
+                         const int MAXIMUM);
 
 #endif //LABO_04_LIBRAIRIE_LIBRAIRIE_H
