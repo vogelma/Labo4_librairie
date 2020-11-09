18fff41273807c1b27f8692ea4ee7422668e1939
valentinchetelat <71428053+valentinchetelat@users.noreply.github.com>
2020-11-09 18:33:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,42 +14,41 @@ Compiler      : Mingw-w64 g++ 8.1.0
 
 using namespace std;
 
-void optionIsEven();
-void optionSumDigits();
-void optionIsPrime();
-void optionNbreArmstrong();
-void optionBuffer();
-void optionTrigo();
-unsigned int askUserForInput_uint(string question, unsigned int min, unsigned int max);
-int askUserForInput_int(string question, int min, int max);
-double askUserForInput_double(string question, double min, double max);
+/*-------------------------------------------------------------------------------
+ * PROTOTYPES
+ -------------------------------------------------------------------------------*/
+void            optionIsEven();
+void            optionSumDigits();
+void            optionIsPrime();
+void            optionNbArmstrong();
+void            optionBuffer();
+void            optionTrigo();
+unsigned int    askUserForInput_uint(string question, unsigned int min, unsigned int max);
+int             askUserForInput_int(string question, int min, int max);
+double          askUserForInput_double(string question, double min, double max);
+void            displayMenu();
 
 int main() {
-    bool repeat = true;
-    int W = 3;
+    bool    repeat          = true;
+    int     menuChoice;
 
     do {
-        int userChoose;
+        /*
+         * DISPLAY MENU
+         */
+        displayMenu();
 
-        //show menu
-        cout << "\nOptions" << endl
-             << "1   is even" << endl
-             << "2   sum digits" << endl
-             << "3   prime number" << endl
-             << "4   Armstrong's number" << endl
-             << "5   random" << endl
-             << "6   buffer" << endl
-             << "7   trigo" << endl
-             << "0   quit" << endl;
+        /*
+         * USER INPUT
+         */
+        menuChoice = askUserForInput_int("\nYour choice ", 0, 7);
 
-        cout << "\nYour choose [0 - 7]:";
-
-        cin >> userChoose;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        switch (userChoose) {
+        /*
+         * FEATURES
+         */
+        switch (menuChoice) {
             case 0:
-                repeat = !answerYes("Would you like to quit [y - n]?", 'y', 'n');
+                repeat = !answerYes("Would you like to quit ", 'y', 'n');
                 break;
             case 1:
                 optionIsEven();
@@ -61,7 +60,7 @@ int main() {
                 optionIsPrime();
                 break;
             case 4:
-                optionNbreArmstrong();
+                optionNbArmstrong();
                 break;
             case 5:
 
@@ -72,73 +71,117 @@ int main() {
             case 7:
                 optionTrigo();
                 break;
-
+            default:
+                break;
         }
 
     } while (repeat);
 
-
+    return EXIT_SUCCESS;
 }
 
+/*-------------------------------------------------------------------------------
+ * FEATURES
+ -------------------------------------------------------------------------------*/
 void optionIsEven() {
     unsigned int number;
 
+    /*
+     * USER INPUT
+     */
     number = askUserForInput_uint("Enter a value ", 0, 1000);
 
+    /*
+     * PRINT WHETHER IT'S PRIME OR NOT
+     */
     cout << number;
     if (isEven(number)) {
-        cout << " is an even value" << endl;
+        cout << " is even" << endl;
     } else {
-        cout << " is not an even value" << endl;
+        cout << " is not even" << endl;
     }
-
 }
 
 void optionSumDigits() {
-    unsigned int result;
-    unsigned int number;
+    unsigned int    number;
+    int             result;
+
+    /*
+     * USER INPUT
+     */
     number = askUserForInput_uint("Enter a value ", 0, 1000);
+
+    /*
+     * DO THE OPERATION
+     */
     result = sumDigits(number);
+
+    /*
+     * PRINT THE RESULT
+     */
     cout << "The sum of all digits in " << number << " = " << result << endl;
 }
 
 void optionIsPrime() {
     unsigned int min;
     unsigned int max;
-    min = askUserForInput_uint("- start: ", 0, 1000);
-    max = askUserForInput_uint("- end  : ", min, 1000);
 
-    for (int i = min; i <= max; ++i) {
+    /*
+     * USER INPUT
+     */
+    min = askUserForInput_uint("- Start: ", 0, 1000);
+    max = askUserForInput_uint("- End  : ", min, 1000);
+
+    /*
+     * PRINT PRIME NUMBERS
+     */
+    for (unsigned int i = min; i <= max; ++i) {
         if (isPrime(i)) {
-            cout << "the number " << i << " is prime" << endl;
+            cout << "The number " << i << " is prime" << endl;
         }
     }
 }
 
-void optionNbreArmstrong() {
+void optionNbArmstrong() {
     unsigned int min;
     unsigned int max;
-    min = askUserForInput_uint("- start: ", 0, 1000);
-    max = askUserForInput_uint("- end  : ", min, 1000);
 
-    for (int i = min; i <= max; ++i) {
-        if (nbreArmstrong(i)) {
-            cout << "the number " << i << " is an Armstrong number" << endl;
+    /*
+     * USER INPUT
+     */
+    min = askUserForInput_uint("- Start: ", 0, 1000);
+    max = askUserForInput_uint("- End  : ", min, 1000);
+
+    /*
+     * PRINT ARMSTRONG NUMBERS
+     */
+    for (unsigned int i = min; i <= max; ++i) {
+        if (nbArmstrong(i)) {
+            cout << "The number " << i << " is an Armstrong number" << endl;
         }
     }
 
 }
 
 void optionBuffer() {
-    char smallestLowerLetter;
-    char biggestUpperLetter;
-    string sentence;
+    char    smallestLowerLetter;
+    char    biggestUpperLetter;
+    string  sentence;
 
+    /*
+     * GET SENTENCE FROM USER
+     */
     cout << "Enter a sentence:";
     getline(cin, sentence);
 
+    /*
+     * DO THE OPERATION
+     */
     int nbChar = buffer(sentence, smallestLowerLetter, biggestUpperLetter);
 
+    /*
+     * PRINT THE RESULT
+     */
     if (smallestLowerLetter != '\0') {
         cout << "The smallest lower letter: " << smallestLowerLetter << endl;
     }
@@ -146,26 +189,37 @@ void optionBuffer() {
         cout << "The biggest upper letter: " << biggestUpperLetter << endl;
     }
 
-    cout << "Number of characters: " << nbChar;
-
+    cout << "Number of characters: " << nbChar << endl;
 }
 
 void optionTrigo() {
-    double sinus;
-    double cosinus;
+    double angle;
+    double sine;
+    double cosine;
     double tangent;
 
-    double angle;
+    /*
+     * USER INPUT
+     */
+    angle = askUserForInput_double("Enter an angle in degree ", 0 , 360);
 
-    angle = askUserForInput_double("Enter an angle in degree: ", 0 , 360);
-    trigo(angle, sinus, cosinus, tangent);
+    /*
+     * DO THE OPERATION
+     */
+    trigo(angle, sine, cosine, tangent);
 
+    /*
+     * PRINT THE RESULT
+     */
     cout << setprecision(6)
-    << "sin(" << angle << ") = " << sinus << endl
-    << "cos(" << angle << ") = " << cosinus << endl
-    << "tan(" << angle << ") = " << tangent << endl;
+         << "sin(" << angle << ") = " << sine << endl
+         << "cos(" << angle << ") = " << cosine << endl
+         << "tan(" << angle << ") = " << tangent << endl;
 }
 
+/*-------------------------------------------------------------------------------
+ * INPUT FUNCTIONS
+ -------------------------------------------------------------------------------*/
 int askUserForInput_int(string question, int min, int max) {
     int userInput;
 
@@ -179,8 +233,9 @@ int askUserForInput_int(string question, int min, int max) {
 
         if (cin.fail()) {
             cin.clear();
-            cout << "wrong input" << endl;
+            cout << "Input error" << endl;
         }
+
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     } while (userInput < min || userInput > max);
@@ -195,14 +250,14 @@ unsigned int askUserForInput_uint(string question, unsigned int min, unsigned in
      *  GET USER INPUT
      */
     do {
-
         cout << question << "[" << min << "-" << max << "] :";
         cin >> userInput;
 
         if (cin.fail()) {
             cin.clear();
-            cout << "wrong input" << endl;
+            cout << "Input error" << endl;
         }
+
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     } while (userInput < min || userInput > max);
@@ -223,11 +278,29 @@ double askUserForInput_double(string question, double min, double max) {
 
         if (cin.fail()) {
             cin.clear();
-            cout << "wrong input" << endl;
+            cout << "Input error" << endl;
         }
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     } while (userInput < min || userInput > max);
 
     return userInput;
+}
+
+/*-------------------------------------------------------------------------------
+ * DISPLAY FUNCTIONS
+ -------------------------------------------------------------------------------*/
+void displayMenu(){
+    /*
+     * DISPLAY THE MENU
+     */
+    cout << "\nOptions" << endl
+         << "1   Is even" << endl
+         << "2   Sum digits" << endl
+         << "3   Prime number" << endl
+         << "4   Armstrong's number" << endl
+         << "5   Random" << endl
+         << "6   Buffer" << endl
+         << "7   Trigo" << endl
+         << "0   Quit" << endl;
 }
